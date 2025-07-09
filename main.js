0e9a1283db9f93ac94bc9d6338484d2777d9790b
UserKit0707 <yuntamaltiqo@gmail.com>
2025-07-09 12:49:04 +0700

    install tailwind and chart Js

---
+++ main.js
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import "./assets/style.css";
+import App from "./App.vue";
+import VueSmoothScroll from "vue3-smooth-scroll";
+import Button from "./components/Button.vue";
+import LinkButton from "./components/LinkButton.vue";
+
+let app = createApp(App);
+app.use(VueSmoothScroll);
+app.mount("#app");
